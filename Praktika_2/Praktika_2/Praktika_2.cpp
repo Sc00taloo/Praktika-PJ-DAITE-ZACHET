d439d5f74aac80adc20f398184ba56e8daaafdd1
Sc00taloo <96819325+Sc00taloo@users.noreply.github.com>
2022-04-14 01:09:37 +0300

    Add files via upload

---
+++ Praktika_2/Praktika_2/Praktika_2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+void prov(double eps) {
+    if (eps < 0.0) {
+        cout << "Error: eps must be posistive!!" << endl;
+        exit(0);
+    }
+    else if (eps > 1.0) {
+        cout << "Error: eps incorrect" << endl;
+        exit(0);
+    }
+}
+
+void schet(int x, double ex) {
+    double y, c = 0;
+    do {
+        y = c;
+        c = exp(x);
+    } while (abs(y - c) >= ex);
+    cout << c;
+}
+
+int main()
+{
+    int x = 0;
+    double eps = 0.0;
+    cout << "Enter X0 for the function y = exp(x): ";
+    cin >> x;
+    cout << endl;
+    cout << "Specify the precision for the function y = exp(x): ";
+    cin >> eps;
+    cout << endl;
+
+    prov(eps);
+    schet(x, eps);
+    return 0;
+}
